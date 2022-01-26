9b47714ecec0a36516975c88c370d95f97aae67d
renars <renars.pulkis@gmail.com>
2022-01-26 15:11:21 +0200

    KKada kluda

--- piedevas.java
+++ piedevas.java
@@ -1,5 +1,27 @@
 package gitUzdPD;
 
-public class piedevas {
+import javax.swing.JOptionPane;
 
+public class piedevas extends pasutijums{	
+	public String extraPiedeva;
+	
+	public piedevas(String topings, String name, String Pica, int sk, String adress, int price) {
+		super(name, Pica, sk, adress, price);
+		this.extraPiedeva=topings;
+	}
+	
+	public void izvadeP() {
+		JOptionPane.showMessageDialog(null, "|   *Tavs sûtijums*   |\n"
+				  + "|   Tavs vârds: "+sVards+"   |\n"
+				  + "|   Pica: "+pizza+" ar"+extraPiedeva+" piedevu   |\n"
+				  + "|   Skaits: "+pSkaits+"   |\n"
+				  + "|   Cena: !!"+cena+"!!   |\n"
+				  + "|   Tava adrese: "+adrese+"   |");
+	}
+	
+	
+	
+	
+	
 }
+
